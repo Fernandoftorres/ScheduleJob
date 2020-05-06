b535df103e6df2157934040c7fecb1aaa858dcc6
Fernando Torres <fernandoftorres@hotmail.com>
2020-05-06 22:41:29 +0100

    Ajuste no diretório dos arquivos.

---
+++ src/ScheduleJOB.Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScheduleJOB.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Home")]
+        public IActionResult Get()
+        {
+            return new OkResult();
+        }
+    }
+}
